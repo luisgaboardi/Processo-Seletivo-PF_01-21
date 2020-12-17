ba0360ea7bf08bcc7609a47dc5bc58d4ca8cf0a5
luisgaboardi <luis.ggaboardi@gmail.com>
2020-12-16 21:44:44 -0300

    Recebe entrada do usuário e responde problema em terminal e csv

--- Problema1.py
+++ Problema1.py
@@ -21,3 +21,39 @@ for linha in dados_brutos:
 	dados[chave] = valor
 
 # Dados preliminares obtidos e armazenados no dicionário _dados_
+
+# Loop permitindo que o usuário entre uma chave que se deseja saber se existe ou não
+entry_loop = True
+while(entry_loop):
+	try:
+		# Ler a entrada
+		chave_desejada = input('> ')
+
+		# Conferir se existe um elemento com a chave entrada
+		# e se o valor dela já não foi adicionado à lista resultado
+		if(chave_desejada in dados):
+			print('Existe um elemento no dicionário com essa chave')
+			if(dados[chave_desejada] not in resultado):
+				print('O valor da chave ({}) foi inserido na lista\n'.format(dados[chave_desejada]))
+				resultado.append(dados[chave_desejada])
+			else:
+				print('Mas o valor da chave ({}) é repetido e não será inserido na lista\n'.format(dados[chave_desejada]))
+		else:
+			print('Não existe elemento no dicionário com essa chave\n')
+
+	# Encerro a loop com CTRL+D
+	except EOFError:
+		entry_loop = False
+
+	# Lida com qualquer erro de memória ou CTRL+C
+	except:
+		exit()
+
+# Escrever a resposta em um novo arquivo csv
+arquivo_csv = 'Resposta1.csv'
+arquivo = open(arquivo_csv, 'w')
+for i in resultado:
+	arquivo.write(i + '\n')
+
+# Printar resultado no terminal
+print("Resultado:\n" + str(resultado))
